95c41b2b4ec7bc71951a15cc969271ada3297c8a
Igor Gorlov <gorlov35@gmail.com>
2021-12-01 16:17:46 +0300

    fix: added nav dissapear animation

--- markup/components/nav/nav.js
+++ markup/components/nav/nav.js
@@ -1,6 +1,7 @@
 class Nav {
     constructor() {
         this.$header = document.querySelector('.header');
+        this.$navWrap = document.querySelector('.header__nav-w');
         this.$navTrigger = document.querySelector('.header__nav-trigger');
         this.$nav = document.querySelector('.nav_primary');
         this.$page = document.querySelector('.page');
@@ -30,6 +31,9 @@ class Nav {
 
     openNav() {
         this.$header.classList.add('header_nav-open');
+        this.$navWrap.classList.add('header__nav-appearance');
+        this.$navWrap.classList.remove('header__nav-disappear');
+
         this.$navTrigger.classList.add('nav__trigger_active');
         this.$nav.classList.add('nav_open');
         this.$page.classList.add('page_nav_open');
@@ -40,6 +44,9 @@ class Nav {
 
     closeNav() {
         this.$header.classList.remove('header_nav-open');
+        this.$navWrap.classList.remove('header__nav-appearance');
+        this.$navWrap.classList.add('header__nav-disappear');
+
         this.$navTrigger.classList.remove('nav__trigger_active');
         this.$nav.classList.remove('nav_open');
         this.$page.classList.remove('page_nav_open');
